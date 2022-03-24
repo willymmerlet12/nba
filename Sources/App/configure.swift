aec1068148e8ea30b1c672e4509bebd9afec6618
willymmerlet12 <willymmerlet@gmail.com>
2022-03-24 19:14:10 -0400

    update config database

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -11,16 +11,28 @@ public func configure(_ app: Application) throws {
         app.middleware.use(file)
         app.routes.defaultMaxBodySize = "10mb"
     
-    app.databases.use(.postgres(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
-    ), as: .psql)
+    if let urlString = Environment.get("DATABASE_URL"),
+       var postgresConfig = PostgresConfiguration(url: urlString) {
+        var tlsConfig = TLSConfiguration.makeClientConfiguration()
+        tlsConfig.certificateVerification = .none
+        postgresConfig.tlsConfiguration = tlsConfig
+        app.databases.use(.postgres(configuration: postgresConfig), as: .psql)
+    } else {
+        app.databases.use(.postgres(
+            hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+            port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
+            username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+            password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+            database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+        ), as: .psql)
+    }
+    
     
     app.migrations.add(CreatePlayers())
-    try app.autoMigrate().wait()
+    
+    if app.environment == .development {
+        try app.autoMigrate().wait()
+    }
 
     // register routes
     try routes(app)
